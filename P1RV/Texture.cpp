e1e7a286473ec3728b16a84a81e032e0b6cdf50f
theodore radu <theodorechristianradu@gmail.com>
2022-01-10 14:20:54 +0100

    Fixed link hair color.

--- P1RV/Texture.cpp
+++ P1RV/Texture.cpp
@@ -10,9 +10,10 @@ void Texture::Load()
 {
 	int width, height, channels;
 	unsigned char* data = stbi_load(path.c_str(), &width, &height, &channels, 0);
+	int mode = (channels == 4) ? GL_RGBA : GL_RGB;
 	glGenTextures(1, &id);
 	glBindTexture(GL_TEXTURE_2D, id);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, mode, GL_UNSIGNED_BYTE, data);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
